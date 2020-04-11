3b90fa5dabc2ae458a849e11c54f1ba2974f4a15
shaheensms <shaheensms111@gmail.com>
2020-04-12 02:13:42 +0600

    Initial commit

---
+++ app/src/main/java/com/metacoders/assistbiker/OTPActivity.java
@@ -0,0 +1,14 @@
+package com.metacoders.assistbiker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OTPActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otp);
+    }
+}
