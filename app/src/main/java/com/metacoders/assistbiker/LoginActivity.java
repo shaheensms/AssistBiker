0ce31b1a690474bea6984ccf5ddd11b8823e4130
rahat14 <rahatshovo6666@gmail.com>
2020-04-13 16:42:49 +0600

    sign up

--- app/src/main/java/com/metacoders/assistbiker/LoginActivity.java
+++ app/src/main/java/com/metacoders/assistbiker/LoginActivity.java
@@ -2,9 +2,11 @@ package com.metacoders.assistbiker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
@@ -23,8 +25,10 @@ import retrofit2.Response;
 public class LoginActivity extends AppCompatActivity {
 
     TextInputEditText mailIn , passIn   ;
-    Button loginBtn ;
+    Button loginBtn  ;
     Utilities utilities  ;
+    TextView registerBtn ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,8 +38,19 @@ public class LoginActivity extends AppCompatActivity {
         mailIn = findViewById(R.id.email) ;
         passIn = findViewById(R.id.pass) ;
         loginBtn = findViewById(R.id.loginBtn);
+        registerBtn = findViewById(R.id.registerBtn) ;
+
 
         utilities = new Utilities() ;
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(getApplicationContext() , SignupActivity.class) ;
+                startActivity(i);
+            }
+        });
 
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/metacoders/assistbiker/SignupActivity.java
+++ app/src/main/java/com/metacoders/assistbiker/SignupActivity.java
@@ -3,12 +3,82 @@ package com.metacoders.assistbiker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class SignupActivity extends AppCompatActivity {
+import com.google.android.material.textfield.TextInputEditText;
+import com.mobsandgeeks.saripaar.ValidationError;
+import com.mobsandgeeks.saripaar.Validator;
+import com.mobsandgeeks.saripaar.annotation.Email;
+import com.mobsandgeeks.saripaar.annotation.NotEmpty;
+import com.mobsandgeeks.saripaar.annotation.Password;
 
+import java.util.List;
+
+public class SignupActivity extends AppCompatActivity implements Validator.ValidationListener {
+
+    @NotEmpty
+    @Email
+    private TextInputEditText mailIn;
+    @NotEmpty
+    private TextInputEditText nameIn , mobileIn;
+
+    @NotEmpty
+    @Password(min = 3, scheme = Password.Scheme.ALPHA_NUMERIC_MIXED_CASE_SYMBOLS)
+    private TextInputEditText passIn;
+
+    Validator validator ;
+    Button signUP ;
+
+
+   //TextInputEditText nameIn , mobileIn    ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        validator = new Validator(this);
+        validator.setValidationListener(SignupActivity.this);
+
+        mailIn = findViewById(R.id.mail) ;
+        passIn = findViewById(R.id.pass) ;
+        nameIn = findViewById(R.id.name) ;
+        mobileIn = findViewById(R.id.ph) ;
+        signUP = findViewById(R.id.signUPBtn) ;
+
+        signUP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                validator.validate();
+            }
+        });
+
+
+
+
+
+    }
+
+    @Override
+    public void onValidationSucceeded() {
+
+        Toast.makeText(this, "message", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onValidationFailed(List<ValidationError> errors) {
+        for (ValidationError error : errors) {
+            View view = error.getView();
+            String message = error.getCollatedErrorMessage(this);
+
+            // Display error messages ;)
+            if (view instanceof TextInputEditText) {
+                ((TextInputEditText) view).setError(message);
+            } else {
+                Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
