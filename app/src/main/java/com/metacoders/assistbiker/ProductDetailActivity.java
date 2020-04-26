a15051e57eda909ddff0a5e70e4dd91308c6f271
shaheensms <shaheensms111@gmail.com>
2020-04-27 00:33:43 +0600

    Product details page initial

---
+++ app/src/main/java/com/metacoders/assistbiker/ProductDetailActivity.java
@@ -0,0 +1,14 @@
+package com.metacoders.assistbiker;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProductDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_detail);
+    }
+}
