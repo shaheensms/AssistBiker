56b8092b952c18dff860295faccb79ee141d2da2
shaheensms <shaheensms111@gmail.com>
2020-05-14 20:30:14 +0600

    Emergency fuel layout designed with row again

--- app/src/main/java/com/metacoders/assistbiker/adapter/CartRecylerViewAdapter.java
+++ app/src/main/java/com/metacoders/assistbiker/adapter/CartRecylerViewAdapter.java
@@ -1,17 +1,13 @@
 package com.metacoders.assistbiker.adapter;
 
 import android.annotation.SuppressLint;
-import android.content.Context;
-import android.content.Intent;
 import android.os.AsyncTask;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
@@ -39,10 +35,7 @@ public class CartRecylerViewAdapter extends RecyclerView.Adapter <CartRecylerVie
     private List<CartDbModel> cartList = new ArrayList<>() ;
     private  CartActivity cartActivity ;
 
-
     Utilities utilities = new Utilities() ;
-
-
 
     private CartRecylerViewAdapter.ClickListener clickListener;
 
