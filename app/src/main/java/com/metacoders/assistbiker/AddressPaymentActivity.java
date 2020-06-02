8b253c0d295f3962d0ec10c0b93b6e77ffd65939
shaheensms <shaheensms111@gmail.com>
2020-06-02 15:27:16 +0600

    AddressPaymentActivity added and designed. row image size reduced.

---
+++ app/src/main/java/com/metacoders/assistbiker/AddressPaymentActivity.java
@@ -0,0 +1,14 @@
+package com.metacoders.assistbiker;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddressPaymentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_address_payment);
+    }
+}
